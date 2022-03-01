dd3e40cce5e8f702647a11db99dd6a8fc48022c2
GabrAlmeid <85247121+GabrAlmeid@users.noreply.github.com>
2022-02-28 21:48:26 -0300

    Commit 6

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
+// Selecionando os filhos
+
 // $("#lista1 > .item2").hide()
 
 /*
 let lista1 = $("#lista1");
 lista1.find(".item2").hide()
 */
-
 // lista1.children()
+
+
+/* Encadeamento
+
+let jq = $("#lista1").slideUp(2000).slideDown(1000)
+                                   .fadeOut(3000)
+
+console.log(jq)
+*/
+/*
+let objeto = {
+    print10: function(){
+        console.log(10);
+        return this;
+    },
+    print20: function(){
+        console.log(20);
+        return this;
+    }
+}
+
+objeto.print10().print20().print10().print20()
+*/
+
+/* Lendo o conteudo de um elemento
+
+console.log($("#lista1").html())
+console.log($("#novo").val())
+console.log($("#novo").attr("name"))
+*/
 
