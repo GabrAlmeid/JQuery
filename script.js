311e60b75c4753250b30ed8a6ff36855f640b429
GabrAlmeid <85247121+GabrAlmeid@users.noreply.github.com>
2022-03-02 20:34:54 -0300

    Commit

--- script.js
+++ script.js
@@ -103,3 +103,16 @@ console.log($("h1").css({"font-size":"50px",
 */
 
 
+
+/* Dimensões
+
+
+let box = $(".box")
+
+box.width(600)
+
+box.append("largura: ", box.width(), "<br>",
+    "largura + padding: ", box.innerWidth(), "<br>",
+    "largura + padding + borda: ", box.outerWidth(), "<br>",
+    "largura + padding + borda + margem: ", box.outerWidth(true))
+*/
